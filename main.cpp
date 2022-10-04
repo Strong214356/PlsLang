386feb50d757ec85df2b7ab3993f9ba8381b0f4e
Strong214356 <tidiandelage1@gmail.com>
2022-10-04 15:58:19 +0200

    finilizing the setup

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ int main(int argc, char* argv[]) {
         std::cerr << "Please specify the .plsSetup file to start" << std::endl;
         return -1;
     }
-    std::cout << "well done you know how to use it\nyou want to use " << argv[1] << " as the setup file!" << std::endl;
+
     return 0;
 }
